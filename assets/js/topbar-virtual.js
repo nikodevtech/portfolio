32674db4f2d7293050d76c1d643b95ab90e609b0
niko <nikoalvarezzapata@gmail.com>
2023-06-18 14:13:51 +0200

    changed img of projects

--- assets/js/topbar-virtual.js
+++ assets/js/topbar-virtual.js
@@ -196,40 +196,6 @@ $(document).ready(function() {
   });
 });
 
-
-/*
- *  Counter
- *
- *  Require(" jquery.animateNumber.min.js ", " jquery.waypoints.min.js ")
- */
-$(document).ready(function() {
-  var counterInit = function() {
-    if ( $('.section-counter').length > 0 ) {
-      $('.section-counter').waypoint( function( direction ) {
-
-        if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-          var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-          $('.number').each(function(){
-            var $this = $(this),
-              num = $this.data('number');
-            $this.animateNumber(
-              {
-                number: num,
-                numberStep: comma_separator_number_step
-              }, 5000
-            );
-          });
-          
-        }
-
-      } , { offset: '95%' } );
-    }
-
-  }
-  counterInit();
-});
-
 const btnDescargar = document.getElementById('btnDescargar');
 btnDescargar.addEventListener('click', function() {
   window.open('CV_Nico.pdf', '_blank');
